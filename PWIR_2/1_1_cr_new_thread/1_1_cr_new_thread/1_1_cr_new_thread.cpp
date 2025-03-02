9f4c59725071a3e669987c7e07ea84c69efcdf26
Michal <michal2000jd@gmail.com>
2025-03-02 20:22:26 +0100

    PWIR_2

---
+++ PWIR_2/1_1_cr_new_thread/1_1_cr_new_thread/1_1_cr_new_thread.cpp
@@ -0,0 +1,24 @@
+﻿#include <cstdio>
+#include <thread>
+#include <windows.h>
+
+void action(int sleep_time) {
+    printf("Uruchamiam watek %d\n", std::this_thread::get_id());
+    Sleep(sleep_time * 1000); // Usypianie na podaną liczbę sekund
+    printf("Koncze watek %d\n", std::this_thread::get_id());
+}
+
+int main() {
+
+    std::thread t1(action, 5);  // Wątek usypia na 5 sekund
+    std::thread t2(action, 10); // Wątek usypia na 10 sekund
+    std::thread t3(action, 15); // Wątek usypia na 15 sekund
+
+    t1.join(); 
+    t2.join();
+    t3.join();
+
+    printf("Koniec programu \r\n");
+
+    return 0;
+}
---
+++ PWIR_2/2_cr_many_threads/2_cr_many_threads/2_cr_many_threads.cpp
@@ -0,0 +1,35 @@
+﻿#include <cstdio>
+#include <thread>
+#include <windows.h>
+#include <vector>
+
+void action(int id) {
+    printf("Uruchamiam watek %d\n", id);
+    Sleep(5 * 1000); // 5 sekund
+    printf("Koncze watek %d\n", id);
+}
+
+int main() {
+    int thread_count;
+
+    printf("Podaj liczbe watkow: ");
+    scanf_s("%d", &thread_count);
+
+    std::vector<std::thread*> threads;
+
+    for (int i = 0; i < thread_count; i++) {
+        threads.push_back(new std::thread(action, i));
+    }
+
+    for (int i = 0; i < thread_count; i++) {
+        threads[i]->join();
+    }
+
+    for (int i = 0; i < thread_count; i++) {
+        delete threads[i];
+    }
+
+    printf("Koniec programu \r\n");
+
+    return 0;
+}
